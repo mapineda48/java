0a7b0401488d89ea4aee0bd226fc57fc4c180cfe
Miguel Pineda <a.pinedavegamiguel@gmail.com>
2021-10-23 15:41:35 -0500

    first commit oauth2

---
+++ ortopedic/src/main/java/ortopedic/controller/OAuth2.java
@@ -0,0 +1,21 @@
+package ortopedic.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OAuth2 extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(a -> a.antMatchers("/", "/index.html", "/error", "/crud/**", "/api/**").permitAll()
+                .anyRequest().authenticated())
+                .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .oauth2Login();
+
+        http.cors().and().csrf().disable();
+    }
+}
